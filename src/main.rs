6b7677998968c0382bf0449cfc8142d8f6def752
kflange <zknsftk1msrk@gmail.com>
2018-12-17 10:19:52 +0900

    fix error handle

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@
 extern crate csv;
 
 use std::io;
+use std::process;
 
 
 fn main() {
     let mut rdr = csv::Reader::from_reader(io::stdin());
 
     for i in rdr.records() {
-        let record = i.expect("a CSV record");
-        println!("{:?}", record);
+        match i {
+            Ok(record) => println!("{:?}", record),
+            Err(err) => {
+                println!("error reading CSV from <stdin>: {}", err);
+                process::exit(1);
+            }
+        }
     }
 }
