cc0af24e54e4610895ffec361b16aa891dd9aa74
kflange <zknsftk1msrk@gmail.com>
2018-12-17 10:33:12 +0900

    add builder

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@
 
 extern crate csv;
 
-use std::env;
 use std::error::Error;
-use std::ffi::OsString;
-use std::fs::File;
+use std::io;
 use std::process;
 
 
@@ -17,19 +15,12 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<Error>> {
-    let file_path = get_first_arg()?;
-    let file = File::open(file_path)?;
-    let mut rdr = csv::Reader::from_reader(file);
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(io::stdin());
     for i in rdr.records() {
         let record = i?;
         println!("{:?}", record);
     }
     Ok(())
-}
-
-fn get_first_arg() -> Result<OsString, Box<Error>> {
-    match env::args_os().nth(1) {
-        None => Err(From::from("expected 1 argument, but got none")),
-        Some(file_path) => Ok(file_path),
-    }
 }
